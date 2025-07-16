0a5c4c7d3591e27aa865c9ed6eee4c92faec317b
Suhail Razzak <suhail.razzak@gmail.com>
2025-07-16 14:58:44 -0400

    fixes

--- queries/injections.scm
+++ queries/injections.scm
@@ -7,9 +7,5 @@
 ((jsx_element) @injection.content
   (#set! injection.language "javascript"))
 
-(
-  (jsx_expression
-    (text) @injection.content
-  )
-  (#set! injection.language "javscript")
-)
+((jsx_expression @injection.content
+  (#set! injection.language "javscript"))
