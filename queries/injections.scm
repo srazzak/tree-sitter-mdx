cb0546405bb706a5b1f7b17d0fea1ae0b7c30161
Suhail Razzak <suhail.razzak@gmail.com>
2025-07-17 22:20:02 -0400

    update injections

--- queries/injections.scm
+++ queries/injections.scm
@@ -7,5 +7,14 @@
 ((jsx_element) @injection.content
   (#set! injection.language "javascript"))
 
-((jsx_expression @injection.content
+((jsx_self_closing_element) @injection.content
+  (#set! injection.language "javascript"))
+
+((jsx_expression) @injection.content
+  (#set! injection.language "javscript"))
+
+((import_statement) @injection.content
+  (#set! injection.language "javscript"))
+
+((export_statement) @injection.content
   (#set! injection.language "javscript"))
